fc5f494d8c6932a0b35786948bc801b6f8980e1b
SumitTr96 <sumitkr9199@gmail.com>
2025-05-14 00:00:02 +0530

    U

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://sumitkr9199:JssEWd3nHUX20uTu@cluster0.pkabura.mongodb.net/", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", () => console.log("Connected to MongoDB!"));
-
-module.exports = db;
+mongoose.connect("mongodb+srv://sumitkr9199:JssEWd3nHUX20uTu@cluster0.pkabura.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.catch((err)=> console.error("Connection error:",err));
